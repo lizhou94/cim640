a9747eb7aebc05ba5f7e073fa31ae84aebd31696
lizhou94 <31257215+lizhou94@users.noreply.github.com>
2017-10-10 20:03:23 -0400

    Week7 HW Practice#3

---
+++ week7Homework/p5-4/empty-example/week7homework.js
@@ -0,0 +1,28 @@
+function setup() {
+                createCanvas(500, 500);
+                background("blue");
+               
+                stroke(0);
+               
+                strokeWeight(5);
+                fill("white")
+                ellipse(250, 250, 150, 150);
+               
+                strokeWeight(5);
+                fill("white")
+                ellipse(250, 409, 175, 175);
+                strokeWeight(5);
+                fill("white")
+                ellipse(250, 115, 120, 120);
+                strokeWeight(5);
+                fill("red")
+                rect(210, 90, 15, 15);
+               
+                
+                strokeWeight(5);
+                fill("red")
+                rect(270, 90, 15, 15);
+                arc(250,150,25,25,170,24.7);
+                
+ 
+                }
