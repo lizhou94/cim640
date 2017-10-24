1ad2a363d83e7301eccaaddd1b9d62c91df6f8a7
lizhou94 <31257215+lizhou94@users.noreply.github.com>
2017-10-24 15:01:11 -0400

    change

---
+++ Homework Folder/week7/week7Homework/p5-4/homework/week7homework.js
@@ -0,0 +1,35 @@
+function setup() {
+                createCanvas(500, 500);
+                background("blue");
+               
+                stroke(0);
+               
+                strokeWeight(5);
+                fill("white")
+                ellipse(250, 250, 150, 150);
+               
+                strokeWeight(5);
+                fill("white")
+                ellipse(250, 409, 175, 175);
+                strokeWeight(5);
+                fill("white")
+                ellipse(250, 115, 120, 120);
+                strokeWeight(3);
+                fill("yellow");
+    
+                stroke("black")
+                triangle(200, 250, 250, 190, 255, 200);
+                strokeWeight(5);
+                fill("red")
+                rect(210, 90, 15, 15);
+    
+                strokeWeight(2);
+                line(250, 80, 250, 100);
+                
+                strokeWeight(5);
+                fill("red")
+                rect(270, 90, 15, 15);
+    
+                arc(250,150,25,25,170,24.7);
+}
+             
